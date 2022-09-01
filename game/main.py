9e09095a7107f22ef6f40f799410a1214e1b94ac
nsh-ningshuohao <105223942+nsh114514@users.noreply.github.com>
2022-09-01 19:51:34 +0800

    Update and rename __init__.py to main.py

---
+++ game/main.py
@@ -0,0 +1,27 @@
+import tkinter
+import time
+win = tkinter.Tk()
+win.resizable(0,0)
+win.title("丐")
+win.geometry("800x600+1+1")
+photo = tkinter.PhotoImage(file="../res/background.png")
+bgd = tkinter.Label(image=photo)
+bgd.place(relx=0.5,rely=0.5,anchor="center")
+son_label = tkinter.Label(win,text="Beggar",font=("Sans Serif",40))
+son_label.place(anchor="center",relx=0.5,rely=0.3,height=60,width=140)
+def singleplayer():
+    import time
+    time = time.time()
+    print("time:"+str(time),"nope")
+def quitgame():
+    import time
+    time = time.time()
+    print("time:"+str(time),"quiting...")
+    win.quit()
+son_button1 = tkinter.Button(win,text="单机游戏",command=singleplayer)
+son_button1.place(anchor="center",relx=0.5,rely=0.4)
+son_button2 = tkinter.Button(win,text="退出",command=quitgame)
+son_button2.place(anchor="center",relx=0.55,rely=0.6)
+time = time.time()
+print("time:"+str(time),"running...")
+win.mainloop()
